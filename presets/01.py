623849f7e41655db3368d4d3fd0ba9db0ec665a4
MaybeBroken <davidsponseller123@gmail.com>
2025-01-23 16:49:53 -0800

    refactor: replace DirectFrame with DirectButton and update properties

--- presets/01.py
+++ presets/01.py
@@ -11,80 +11,10 @@ class frame:
     def build(self):
         ...
 
-        self.obj0 = DirectFrame(
+        self.obj0 = DirectButton(
             parent=self.aspect2d,
-            pos=LPoint3f(0, 0, 0),
-            scale=LVecBase3f(0.42196416, 0, 0.42196416),
-            color=(1, 1, 1, 1),
-            relief=None,
-            geom=None,
-            image=self.loader.loadTexture("01/textures/frame2.png"),
-        )
-
-        self.obj2 = DirectFrame(
-            parent=self.aspect2d,
-            pos=LPoint3f(-0.12, 0, 0.32),
-            scale=LVecBase3f(0.84964305, 0, 0.57294654),
-            color=(1, 1, 1, 1),
-            relief=None,
-            geom=None,
-            image=self.loader.loadTexture("01/textures/frame2.png"),
-        )
-
-        self.obj4 = DirectButton(
-            parent=self.aspect2d,
-            pos=LPoint3f(-0.75, 0, 0.67),
-            scale=LVecBase3f(0.119360074, 0, 0.119360074),
-            color=(1, 1, 1, 1),
-            relief=None,
-            geom=None,
-            image=self.loader.loadTexture("01/textures/button1.png"),
-        )
-
-        self.obj6 = DirectButton(
-            parent=self.aspect2d,
-            pos=LPoint3f(-0.36, 0, 0.67),
-            scale=LVecBase3f(0.119360074, 0, 0.119360074),
-            color=(1, 1, 1, 1),
-            relief=None,
-            geom=None,
-            image=self.loader.loadTexture("01/textures/button1.png"),
-        )
-
-        self.obj8 = DirectButton(
-            parent=self.aspect2d,
-            pos=LPoint3f(0.08, 0, 0.67),
-            scale=LVecBase3f(0.119360074, 0, 0.119360074),
-            color=(1, 1, 1, 1),
-            relief=None,
-            geom=None,
-            image=self.loader.loadTexture("01/textures/button1.png"),
-        )
-
-        self.obj10 = DirectButton(
-            parent=self.aspect2d,
-            pos=LPoint3f(-0.77, 0, 0.33),
-            scale=LVecBase3f(0.119360074, 0, 0.119360074),
-            color=(1, 1, 1, 1),
-            relief=None,
-            geom=None,
-            image=self.loader.loadTexture("01/textures/button1.png"),
-        )
-
-        self.obj12 = DirectButton(
-            parent=self.aspect2d,
-            pos=LPoint3f(-0.35, 0, 0.33),
-            scale=LVecBase3f(0.119360074, 0, 0.119360074),
-            color=(1, 1, 1, 1),
-            relief=None,
-            geom=None,
-            image=self.loader.loadTexture("01/textures/button1.png"),
-        )
-
-        self.obj14 = DirectButton(
-            parent=self.aspect2d,
-            pos=LPoint3f(0.09, 0, 0.33),
-            scale=LVecBase3f(0.119360074, 0, 0.119360074),
+            pos=LPoint3f(-0.31, 0, 0.42),
+            scale=LVecBase3f(0.09, 0.09, 0.09),
             color=(1, 1, 1, 1),
             relief=None,
             geom=None,
